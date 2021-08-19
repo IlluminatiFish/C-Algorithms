2af8bae923c7f0db4a03d2474537da14d2e2ce41
IlluminatiFish <45714340+IlluminatiFish@users.noreply.github.com>
2021-08-19 23:55:59 +0100

    Update bin2dec.c
    Updated binary value variable name to allow for more readability in the future

--- bin2dec.c
+++ bin2dec.c
@@ -1,32 +1,32 @@
 #include <math.h>
 #include <stdio.h>
 
-int convert(long long n);
+int convert(long long bin_value);
 
 int main() {
     
-    long long n;
+    long long bin_value;
     
     printf("Enter a binary number: \n");
     
     printf("Binary Value: ");
-    scanf("%lld", &n);
+    scanf("%lld", &bin_value);
     
-    printf("%lld is %d in decimal", n, convert(n));
+    printf("%lld is %d in decimal", bin_value, convert(bin_value));
     
     return 0;
 }
 
 
-int convert(long long n) {
+int convert(long long bin_value) {
     
     int decimal = 0;
     int iterator = 0;
     int remainder;
     
-    while (n != 0) {
-        remainder = n % 10;
-        n /= 10;
+    while (bin_value != 0) {
+        remainder = bin_value % 10;
+        bin_value /= 10;
         decimal += remainder * pow(2, iterator);
         ++iterator;
     }
